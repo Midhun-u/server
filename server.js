0b158d1b4224962d7a361eb3f505e7ced9038696
Midhun-u <midhunvkm689@gmail.com>
2025-04-13 19:33:44 +0530

    Changed everything

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from 'express'
 const PORT = 3000
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 app.get("/" , (request , response) => {
     
     const data = {
